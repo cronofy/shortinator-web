d29d27bfab2c7e2599cc74d3d5dfd444954ed3b0
Adam Bird <adam.bird@gmail.com>
2014-02-05 19:37:30 +0000

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "http://rubygems.org"
+
+gem 'unicorn'
+gem 'sinatra'
+gem 'hatchet'
+gem 'shortinator', :git => "https://github.com/adambird/shortinator.git"
---
+++ app.rb
@@ -0,0 +1,36 @@
+require "bundler/setup"
+require 'sinatra'
+require 'shortinator'
+
+require 'hatchet'
+
+register Hatchet
+
+Hatchet.configure do |config|
+  config.level :info
+end
+
+IGNORE = ['favicon.ico']
+
+def html_wrapper(content)
+  "<html><body>#{content}</body></html>"
+end
+
+get '/' do
+  [ 200, {}, html_wrapper("Shortinator") ]
+end
+
+get '/:id' do
+  if IGNORE.include?(params[:id])
+    return [200, {}, ""]
+  end
+
+  begin
+    redirect_to_url = Shortinator.click(params[:id], "0.0.0.0")
+    logger.info "redirect_to_url=#{redirect_to_url}"
+    [ 302, { "Location" => redirect_to_url }, html_wrapper("<a href=\"#{redirect_to_url}\">#{redirect_to_url}</a>")]
+  rescue => e
+    logger.error e.message
+    [ 404, {}, html_wrapper("Not found") ]
+  end
+end
