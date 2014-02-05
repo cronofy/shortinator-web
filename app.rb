c1e0bbf1853f977d02522f03763a30e1e0479150
Garry Shutler <garry@robustsoftware.co.uk>
2014-02-05 22:17:25 +0000

    Use Sinatra's pass method

--- app.rb
+++ app.rb
@@ -15,20 +15,18 @@ def html_wrapper(content)
 end
 
 get '/' do
-  [ 200, {}, html_wrapper("Shortinator") ]
+  [200, {}, html_wrapper("Shortinator")]
 end
 
 get '/:id' do
-  if IGNORE.include?(params[:id])
-    return [200, {}, ""]
-  end
+  pass if IGNORE.include?(params[:id])
 
   begin
     redirect_to_url = Shortinator.click(params[:id], "0.0.0.0")
-    logger.info "redirect_to_url=#{redirect_to_url}"
-    [ 302, { "Location" => redirect_to_url }, html_wrapper("<a href=\"#{redirect_to_url}\">#{redirect_to_url}</a>")]
+    log.info { "redirect_to_url=#{redirect_to_url}" }
+    [302, { "Location" => redirect_to_url }, html_wrapper("<a href=\"#{redirect_to_url}\">#{redirect_to_url}</a>")]
   rescue => e
     logger.error e.message
-    [ 404, {}, html_wrapper("Not found") ]
+    [404, {}, html_wrapper("Not found")]
   end
 end
